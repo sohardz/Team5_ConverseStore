abbe62dd91877919129ec53b8a9def778367bd91
sohardz <sohardz01@gmail.com>
2023-05-16 11:36:26 +0700

    create folder configurations

---
+++ Shop.Data/Configuations/ChucVuConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Shop.Data.Models;
+
+namespace Shop.Data.Configuations;
+public class ChucVuConfiguration : IEntityTypeConfiguration<ChucVu>
+{
+    public void Configure(EntityTypeBuilder<ChucVu> builder)
+    {
+        builder.HasKey(p => p.Id);
+        builder.Property(p => p.Ten).HasColumnType("varchar(100)");
+    }
+}
